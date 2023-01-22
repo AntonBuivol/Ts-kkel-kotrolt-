51126b81ef06aeb8d649a646b08814a871fee64b
AntonBuivol <anton.buivol@gmail.com>
2023-01-22 19:04:34 +0200

    Variant 5

--- Tsükkel kotroltöö/Tsükkel_kotroltöö.py
+++ Tsükkel kotroltöö/Tsükkel_kotroltöö.py
@@ -1,36 +1,48 @@
 #5 variant
 import random
 
-#1
-#n=int(input("Kirjutage täisarv vahemikus 1 kuni 9: "))
-#a1= " ~~~~~     "
-#a2="/_____\    "
-#a3="| []  |    "
-#a4= " -----     "
-#x=0
-#while x<n:
-#    if n>0 and n<10:
-#        print(a1*n,a2*n,a3*n,a4*n,sep="\n")
-#        break
-#    else:
-#        print("Sul oli vaja kijuta täisarv vahemikus 1 kuni 9!")
-
-#2
-õpilased=int(input("Inimeste arv esimesel klassis: "))
-õpilased2=õpilased
-klass=0
-klass2=0
-for x in range(õpilased):
-    hind=random.randint(1,5)
-    klass+=hind
-for y in range(õpilased2):
-        hind2=random.randint(1,5)
-        klass2+=hind2
-keskminehind=round(klass/õpilased,1)
-keskminehind2=round(klass2/õpilased,1)
-print("Esimesel klassis keskmine hind: ",keskminehind)
-print("Teisel klassis keskmine hind: ",keskminehind2)
+#Ülusanne 1
+n=int(input("Sisestage majade arv 1 kuni 9: "))
+maja="""
+     ~~~~~     
+    /_____\\    
+    | []  |    
+     -----     
+"""
+if n>0 and n<10:
+    for i in range(n):
+        if n>0 and n<9:
+            print(maja, end=" ")
+else:
+    print("Sisestage arv vahemikus 1 kuni 9")
 
 
-#4
-#people=random.randint()
+#Ülesanne 2
+#print("Klassides on 10 õpilased")
+#õpilased=10
+#hinnad=[3, 4, 5, 2, 4, 5, 5, 4, 3, 5]
+#hinnad2=[4, 5, 4, 3, 4, 2, 1, 3, 5, 5]
+#klass=0
+#klass2=0
+#for i in range(õpilased):
+#    klass+=hinnad[i]
+#    klass2+=hinnad2[i]
+#keskmine_hind=klass/õpilased
+#keskmine_hind2=klass2/õpilased
+#print("Esimesel klassis keskmine hind: ",keskmine_hind)
+#print("Teisel klassis keskmine hind: ",keskmine_hind2)
+
+#Ülusanne 3
+#õpilased=random.randint(15, 30)
+#min_hinne=5
+#max_hinne=0
+#for i in range(õpilased):
+#    hinne=random.randint(1,5)
+#    if hinne<min_hinne:
+#        min_hinne=hinne
+#    if hinne>max_hinne:
+#        max_hinne=hinne
+
+#print(f"Klassis on {õpilased} õpilased")
+#print(f"Minimaalne hinne {min_hinne}")
+#print(f"Maksimaalne hinne {max_hinne}")
